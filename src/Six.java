6b0e8c1006b7ed192b96ce83ef0fe59cd67f8eb7
youngho kim <kyh79@inka.co.kr>
2016-12-01 18:46:51 +0900

    commit number 4

---
+++ src/Six.java
@@ -0,0 +1,26 @@
+public class Six {
+	String message = "foo";
+	String message1 = "foo1";
+	String message2 = "foo1";
+	String message3 = "foo1";
+	
+	public String foo(){
+		return message;
+	}
+
+	public void uncoveredMethod(){
+		System.out.println(foo());
+	}
+	
+	public String foo1(){
+		return message1;
+	}
+	
+	public String foo2(){
+		return message2;
+	}
+	
+	public String foo3(){
+		return message3;
+	}
+}
